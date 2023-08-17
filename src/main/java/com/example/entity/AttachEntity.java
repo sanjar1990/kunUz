c0530d91205ba1b558af0148a1e986905adabd1f
sanjar1990 <sanjar902105@gmail.com>
2023-08-17 13:27:01 +0500

    errors are fixed

--- src/main/java/com/example/entity/AttachEntity.java
+++ src/main/java/com/example/entity/AttachEntity.java
@@ -1,7 +1,7 @@
 package com.example.entity;
 
 import jakarta.persistence.*;
-<<<<<<< HEAD
+
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
@@ -32,31 +32,6 @@ public class AttachEntity {
     private String extension;
 
     @Column(name = "created_date")
-    private LocalDateTime createdData;
-=======
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Entity()
-@Table(name = "attach")
-public class AttachEntity {
-    @Id
-    private String id;
-    @Column
-    private String originalName;
-    @Column
-    private String path;
-    @Column
-    private Long size;
-    @Column
-    private String extension;
-    @Column
     private LocalDateTime createdDate=LocalDateTime.now();
->>>>>>> attachPractise
+
 }
--- src/main/java/com/example/repository/AttachRepository.java
+++ src/main/java/com/example/repository/AttachRepository.java
@@ -1,19 +1,12 @@
 package com.example.repository;
 
-<<<<<<< HEAD
 
-import com.example.entity.AttachEntity;
-import org.springframework.data.repository.CrudRepository;
 
-public interface AttachRepository extends CrudRepository<AttachEntity,String > {
-=======
 import com.example.entity.AttachEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface AttachRepository extends CrudRepository<AttachEntity, String >,
-        PagingAndSortingRepository<AttachEntity, String> {
-
->>>>>>> attachPractise
+public interface AttachRepository extends CrudRepository<AttachEntity,String >,
+        PagingAndSortingRepository<AttachEntity,String> {
 
 }
